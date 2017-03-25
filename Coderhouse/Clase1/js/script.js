6d1930c49da12e3a42e5f24267cd26355ed4d375
Juanpernu <juan.pernumian@gmail.com>
2017-03-25 12:31:11 -0300

    Clase 1

---
+++ Coderhouse/Clase1/js/script.js
@@ -0,0 +1,54 @@
+// // console.log('Hola!');
+
+// /*
+// //////// OPERADORES MATEMATICOS ////////
+
+//   + - / *
+
+// */
+
+// var Nombre = 'Juan';
+// var Edad = 26;
+
+// console.log(Nombre);
+// console.log(Edad / 2);
+
+// // CONTATENACION DE STRINGS
+// console.log(Nombre + "Lucia");
+
+// // TIPO DE CONTENIDO
+// console.log(
+//   typeof(Edad)
+// );
+
+/*
+/////// EJ 1
+var Nombre = prompt("Ingrese su nombre");
+console.log("Hola " + Nombre);
+*/
+
+/*
+/////// EJ 2
+var numeroUno = prompt("Ingrese un valor");
+var numeroDos = 2;
+alert(numeroUno + numeroDos);
+*/
+
+/*
+/////// EJ 3
+var numeroUno = prompt("Ingrese un valor");
+var numeroDos = 3;
+alert(numeroUno - numeroDos);
+*/
+
+/*
+/////// EJ 4
+  CASTEO DE VARIABLES -> parseInt(); ó Number();
+var numeroUno = prompt("Ingrese un valor");
+var numeroDos = prompt("Ingrese otro valor");
+
+var parsedNumero1 = parseInt(numeroUno);
+var parsedNumero2 = parseInt(numeroDos);
+
+alert(parsedNumero1 + parsedNumero2);
+*/
